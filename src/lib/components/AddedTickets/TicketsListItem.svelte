3cc3e792acdf0097fcdc15ea75df3bace706ab95
2ulqarnain <zulqernaynh@gmail.com>
2025-06-29 10:31:24 +0500

    styles

--- src/lib/components/AddedTickets/TicketsListItem.svelte
+++ src/lib/components/AddedTickets/TicketsListItem.svelte
@@ -13,16 +13,17 @@
 	let { ticket, itemIndex, onDeleteTicket }: Props = $props();
 </script>
 
-<li class="group relative -mb-20 transition-[margin] hover:mb-0 last:hover:-mb-20">
-	<code class="absolute top-2 left-0 -translate-x-[150%] text-zinc-500">{itemIndex}.</code>
+<li class="group relative -mb-20 transition-[margin] hover:-mb-3 last:hover:-mb-20">
+	<code class="absolute top-2 left-0 -translate-x-[150%] text-zinc-400">{itemIndex}.</code>
 	<div
-		class="row-span-2 grid grid-cols-1 grid-rows-[auto_1fr_auto] items-center gap-2 overflow-hidden rounded-2xl border border-zinc-400 bg-gray-300"
+		class="row-span-2 grid grid-cols-1 grid-rows-[auto_1fr_auto] items-center gap-2 overflow-hidden rounded-2xl border border-zinc-400 bg-gray-300 shadow-[0_-10px_20px_#0001]"
 	>
 		<div class="flex w-full gap-2 p-2">
 			<code class="text-foreground-muted font-semibold">{ticket.id}</code>
 			<span
 				class="text-foreground-light w-fit rounded-xl px-2"
-				style="background-color:{ticketStatusColorMap[ticket.status]};">{ticket.status}</span
+				style="background-color:{ticketStatusColorMap[ticket.status] ?? 'gray'};"
+				>{ticket.status}</span
 			>
 			<TimeLog className="ml-auto" startedOn={ticket.startedOn} />
 		</div>
